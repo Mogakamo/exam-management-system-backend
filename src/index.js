b37984163f4abe3f003f1515f3a55cbb9890db56
Mogakamo <mogaka.amo254@gmail.com>
2022-05-30 22:22:12 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 //index.js
 const express = require("express");
-cosnt v1Router = require("./v1/routes")
+const v1Router = require("./v1/routes")
 
 const app = express();
 const PORT = process.env.PORT || 4040;
 
-app.use("/api/v1", v1Router)
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
 	console.log(`API is listening on port ${PORT}`);
