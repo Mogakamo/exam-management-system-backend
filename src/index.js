f259f21ccdbd8626e106974ca6979aea856392cd
Mogakamo <mogaka.amo254@gmail.com>
2022-06-05 22:13:09 +0300

    Adding a cache to improve the perfomance of the api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 //index.js
 const express = require("express");
+
+const bodyParser = require("body-parser");
 const v1ClassRouter = require("./v1/routes/classRoutes");
 
 
 const app = express();
 const PORT = process.env.PORT || 4040;
 
+app.use(bodyParser.json());
 app.use("/api/v1/classes", v1ClassRouter);
 
 app.listen(PORT, () => {
